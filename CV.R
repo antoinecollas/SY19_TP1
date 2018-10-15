8e2fb9ff5b5cedb4d24fe0e2ef4b606ae52cb576
Antoine Collas <antoinecollas@macbook-pro-de-antoine.home>
2018-10-15 14:42:57 +0200

    factorisation du code de validation croisée

---
+++ CV.R
@@ -0,0 +1,38 @@
+require(MASS)
+library(e1071)
+
+#fonction réalisant la validation croisée (avec K=10 par défaut) 
+CV_eval <- function(model, data, fold=10){
+  K <- fold
+  n <- nrow(data)
+  folds <- sample(1:K,n,replace=TRUE)
+  mean <- 0
+  errors <- rep(0, K)
+  for(k in (1:K)){
+    if ((model=='adl') || (model=='adq')){
+      if (model=='adl'){
+        model.fit <- lda(formula=as.factor(y)~., data=data[folds!=k,])
+      }else{
+        model.fit <- qda(formula=as.factor(y)~., data=data[folds!=k,])
+      }
+      pred <- predict(model.fit, data[folds==k,])
+      errors[k] <- sum(data$y[folds==k]!=pred$class) / length(pred$class)
+      mean <- mean + (errors[k]*length(pred$class))
+    }else if ((model=='bayesien_naif') || (model=='reg_logistique')){
+      if (model=='bayesien_naif'){
+        model.fit <- naiveBayes(formula=as.factor(y)~., data=data[folds!=k,])
+        pred <- predict(model.fit, data[folds==k,])
+      }else{
+        glm.fit <- glm(as.factor(y)~., data=data[folds!=k,], family=binomial)
+        pred <- predict(glm.fit, newdata=data[folds==k,], type='response')>0.5
+      }
+      errors[k] <- sum(data$y[folds==k]!=pred) / length(pred)
+      mean <- mean + (errors[k]*length(pred))
+    }else{
+      stop('Le modèle demandé n\'est pas implémenté!')
+    }
+  }
+  mean <- mean/n
+  sd <- sqrt((1/(K-1))*sum((errors - rep(mean, K))^2))
+  return(c(mean, sd))
+}
