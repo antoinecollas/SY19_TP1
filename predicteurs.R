5e1841aa03ee137a107fb16290e987c2b7377d96
BOB-Henoik <36737976+BOB-Henoik@users.noreply.github.com>
2018-10-22 23:10:52 +0200

    ajout de classifieur

--- predicteurs.R
+++ predicteurs.R
@@ -1,6 +1,17 @@
+require(MASS)
+library(FNN)
+library(leaps)
+library(e1071)
+
 regresseur <- function(dataset){
-  load("env_regresseur.Rdata")
+  load("env.Rdata")
   x <- model.matrix(~.-1, data = dataset)
   y <- predict(reg, newx=x)
   return(y)
+}
+
+classifieur <- function(dataset){
+  load("env.Rdata")
+  z <- predict(class.mod, dataset)
+  return(z)
 }
